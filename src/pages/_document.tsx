bfcc8fd1e66ad7cd60ce72e4a1c1d99d057ecefb
yusufyazici01 <yusufyzzc1@gmail.com>
2025-05-04 23:13:48 +0200

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="tr">
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          
+          {/* Favicon bağlantıları */}
+          <link rel="icon" href="/favicon.png" />
+          <link rel="apple-touch-icon" href="/favicon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          {/* Tarayıcı tema rengi */}
+          <meta name="theme-color" content="#004976" />
         </Head>
         <body>
           <Main />
